2fa37f57697e1626d56076292a3b980932afe6fd
betay1 <betelhemaynalem0@gmail.com>
2024-03-24 12:59:37 -0700

    ssh task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,13 +6,13 @@ file { '/etc/ssh/ssh_config':
 }
 
 file_line { 'Turn off passwd auth':
-path => '/etc/ssh/ssh_config',
-line => 'passwordAuthentication no',
-match => '^#passwordAuthentication',
+	path => '/etc/ssh/ssh_config',
+	line => 'passwordAuthentication no',
+	match => '^#passwordAuthentication',
 }
 
 file_line { 'Declare identity file':
-path => '/etc/ssh/ssh_config',
-line => 'IdentityFile ~/.ssh/school',
-match => '^#IdentityFile',
+	path => '/etc/ssh/ssh_config',
+	line => 'IdentityFile ~/.ssh/school',
+	match => '^#IdentityFile',
 }
